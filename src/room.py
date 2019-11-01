fc47ecfca6b9d2dc635999daf59df23d16d3b2d3
angel-torres <alas4224@gmail.com>
2019-10-31 22:15:16 -0500

    finished getting player to move when user inputs direction

--- src/room.py
+++ src/room.py
@@ -2,10 +2,15 @@
 # description attributes.
 
 class Room:
-    def __init__(self, *args):
-        self.args = args
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
 
     def __str__(self):
-        s = f"Room: Description: {self.args}"
+        s = f"{self.name}: {self.description}"
         return s
 
